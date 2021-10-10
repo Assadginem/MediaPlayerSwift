247e2f228b98e4cbcd89d9e45f5d9771da439f8c
Assad Ginem <aginem7078@gmail.com>
2021-10-10 10:51:53 -0500

    Initial Commit

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Assad Ginem on 10/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
